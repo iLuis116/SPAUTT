d0164581c591ae091151028bac4d609de14268c4
Jose Luis Nava Flores <luisflores2103@gmail.com>
2022-11-25 12:59:03 -0600

    navbar del SPA

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const mainContent = document.querySelector('#container');
+const navbar = document.querySelector('.navbar');
+
+const loadData = e => {
+    e.preventDefault();
+    if(e.target.classList.contains('') || e.target.classList.contains('')){
+        
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    fetch('../pages/home.html')
+    .then(page => {
+        return page.text();
+    })
+    .then(contentHtml => {
+        const div = document.createElement('div');
+        div.innerHTML = contentHtml;
+        mainContent.appendChild(div);
+    })
+
+navbar.addEventListener('click', loadData);
+
+})
